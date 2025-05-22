97f55b52767659ec8cdbe29e7b012fbc55c744c7
Artem <abobys388@gmail.com>
2025-05-22 15:35:44 +0300

    fix bag

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -9,7 +9,7 @@ const toggleSidebar = () => {
 }
 
 const navItems = [
-  {icon: Home, label: 'Главная', to: '/home'},
+  {icon: Home, label: 'Главная', to: '/'},
   {icon: User, label: 'Профиль', to: '/profile'},
   {icon: BookOpen, label: 'Курсы', to: '/courses'},
   {icon: ListChecks, label: 'Задачи', to: '/tasks'}
